05fdcb3eddf9d96ea14916b36a93d02c44573187
nshr8815 <nic.iain.schroeder@gmail.com>
2019-05-03 12:57:49 -0600

    Update: Two Users and Server login messages across terminals

--- ChatServer/src/com/muc/ServerMain.java
+++ ChatServer/src/com/muc/ServerMain.java
@@ -12,18 +12,7 @@ public class ServerMain {
 
     public static void main(String[] args) {
         int port = 8818;
-        try {
-
-            ServerSocket serverSocket = new ServerSocket(port);
-            while (true) {
-                System.out.println("Accepting Connection...");
-                Socket clientSocket = serverSocket.accept();
-                System.out.println("Accepted connection from" + clientSocket);
-                ServerWorker worker = new ServerWorker(clientSocket);
-                worker.start();
-        }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        Server server = new Server(port);
+        server.start();
         }
 }
--- ChatServer/src/com/muc/ServerWorker.java
+++ ChatServer/src/com/muc/ServerWorker.java
@@ -5,12 +5,17 @@ import org.apache.commons.lang3.StringUtils;
 import java.io.*;
 import java.net.Socket;
 import java.util.Date;
+import java.util.List;
 
 
 public class ServerWorker extends Thread{
     private final Socket clientSocket;
+    private final Server server;
+    private String login = null;
+    private OutputStream outputStream;
 
-    public ServerWorker(Socket clientSocket){
+    public ServerWorker(Server server, Socket clientSocket){
+        this.server = server;
         this.clientSocket = clientSocket;
     }
 
@@ -25,7 +30,7 @@ public class ServerWorker extends Thread{
 
     }  private void clientHandle() throws IOException, InterruptedException {
         InputStream inputStream = clientSocket.getInputStream();
-        OutputStream outputStream = clientSocket.getOutputStream();
+        this.outputStream = clientSocket.getOutputStream();
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
         String line;
@@ -33,14 +38,12 @@ public class ServerWorker extends Thread{
             //Had to download apache refer to point 1
             String[] tokens = StringUtils.split(line);
             if (tokens != null && tokens.length > 0) {
-
-
                 String cmd = tokens[0];
                 if ("quit".equalsIgnoreCase(line)) {
                     break;
-                } else if ("login".equalsIgnoreCase(cmd))
+                } else if ("login".equalsIgnoreCase(cmd)){
                     handleLogin(outputStream, tokens);
-            }else
+                }else
                 {
                 String msg = "unknown: " + cmd + "\n";
                 outputStream.write(msg.getBytes());
@@ -49,6 +52,38 @@ public class ServerWorker extends Thread{
         }
 
         clientSocket.close();
+    }
+
+
+    public String getLogin() {
+        return login;
+    }
+
+    private void handleLogin(OutputStream outputStream, String[] tokens) throws IOException {
+        if (tokens.length == 3){
+            String login = tokens[1];
+            String password = tokens[2];
+
+            if((login.equals("guest") && password.equals("guest")) || (login.equals("SupremeOverlord") && password.equals("adminBest"))){
+                String msg = "Login Valid" + "\n";
+                outputStream.write(msg.getBytes());
+                this.login = login;
+                System.out.println("User " + login + " logged in succesfully" );
+
+                String onlineMsg = "User " +login + " is now online " + "\n";
+                List<ServerWorker> workerList = server.getWorkerList();
+                for (ServerWorker worker : workerList){
+                    worker.send(onlineMsg);
+                }
+            }else{
+                String msg = "Login Invalid" + "\n";
+                outputStream.write(msg.getBytes());
+            }
+        }
+    }
+
+    private void send(String msg) throws IOException {
+        outputStream.write(msg.getBytes());
     }
 
 
